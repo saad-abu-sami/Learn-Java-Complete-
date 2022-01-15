43d644803603ce192ac2ef5084f6cd75e958b007
Saad Abu Sami <saadabusami061@gmail.com>
2022-01-16 02:12:52 +0600

    Alhamdulilah

--- GFG & JP & Solo Learn JAVA/src/math/Math_1.java
+++ GFG & JP & Solo Learn JAVA/src/math/Math_1.java
@@ -11,6 +11,19 @@ public class Math_1 {
         System.out.println("Absolute value of y is : " + absolute);
         double power = Math.pow(y, x);
         System.out.println("Power of y^x is : " + power);
-
+        System.out.println("Square root of 64 is: " + Math.sqrt(64));
+        System.out.println("A random math number " +Math.random());
+        // return the logarithm of given value
+        System.out.println("Logarithm of x is: " + Math.log(x));
+        System.out.println("Logarithm of y is: " + Math.log(y));
+        // return the logarithm of given value when base is 10
+        System.out.println("log10 of x is: " + Math.log10(x));
+        System.out.println("log10 of y is: " + Math.log10(y));
+        // return the log of x + 1
+        System.out.println("log1p of x is: " +Math.log1p(x));
+        // return x power of 2
+        System.out.println("exp of x is: " +Math.exp(x));
+        // return (x power of 2)-1
+        System.out.println("expm1 of x is: " +Math.expm1(x));
     }
 }
